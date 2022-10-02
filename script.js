b8493ff0bc4d711cb3a5582616c297a7fe48f726
William Rodrigues <williamrodrigues2442@gmail.com>
2022-10-02 18:24:19 -0300

    chore: inital commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const $table = document.getElementById("tabela");
+
+const URL =
+  "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
+
+async function getAndSetData() {
+  const r = await fetch(URL);
+  const data = await r.json();
+  let result = "";
+  data.cand.forEach((cand, index) => {
+    result += `
+      <tr id="${index}">
+        <td>${cand.nm}</td>
+        <td>${cand.cc.slice(0, 20)}...</td>
+        <td>${cand.vap}</td>
+        <td>${cand.pvap}%</td>
+      </tr
+    `;
+  });
+  $table.innerHTML = result;
+}
+
+getAndSetData()
+setInterval(getAndSetData, 10000);
